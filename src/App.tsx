70d59f6ee29d653cdb542bc489cdee81ea5b79c4
Bang <bangjoody@gmail.com>
2022-02-26 14:57:33 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { Refine } from "@pankod/refine-core";
+import { notificationProvider, LoginPage } from "@pankod/refine-antd";
+import routerProvider from "@pankod/refine-react-router-v6";
+import "@pankod/refine-antd/dist/styles.min.css";
+import { dataProvider } from "@pankod/refine-supabase";
+
+import authProvider from "./authProvider";
+import { supabaseClient } from "utility";
+import {
+  Title,
+  Header,
+  Sider,
+  Footer,
+  Layout,
+  OffLayoutArea,
+} from "components/layout";
+import { useTranslation } from "react-i18next";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      notificationProvider={notificationProvider}
+      dataProvider={dataProvider(supabaseClient)}
+      authProvider={authProvider}
+      LoginPage={LoginPage}
+      Title={Title}
+      Header={Header}
+      Sider={Sider}
+      Footer={Footer}
+      Layout={Layout}
+      OffLayoutArea={OffLayoutArea}
+      i18nProvider={i18nProvider}
+    />
+  );
+}
+
+export default App;
