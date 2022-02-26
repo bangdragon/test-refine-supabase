80c70c7572f0a64144518bb62447d467156ade86
Bang <bangjoody@gmail.com>
2022-02-26 16:06:30 +0700

    update files

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@pankod/refine-supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://wqixyunkzhnfswvclhup.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMTgwMjMwMiwiZXhwIjoxOTQ3Mzc4MzAyfQ.0o0LmOfStllVWQJpivzU74gqSo4TUsg_zjkTlL_bvbI";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
